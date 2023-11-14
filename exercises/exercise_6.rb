fad2f7b727f77dd92cc2dd5aa3fe7b8c5a25be4f
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-14 13:13:30 -0500

    create employee data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create!(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create!(first_name: "Bob", last_name: "Bob", hourly_rate: 40)
+@store2.employees.create!(first_name: "Alice", last_name: "Eclia", hourly_rate: 50)
