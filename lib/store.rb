7c39092a98c223e68e7bbe56b091c6c8323ef04a
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-14 13:14:28 -0500

    create association and validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,6 @@
 class Store < ActiveRecord::Base
+  has_many :employees
 
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, presence: true, numericality: { greater_than: 0 }
 end
