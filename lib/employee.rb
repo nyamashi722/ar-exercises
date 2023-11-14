35bebc77ae90fecfe75dbfad179e8e67d00f7e65
Nathan Yamashiro <n.yamashiro722@gmail.com>
2023-11-14 13:14:18 -0500

    create association and validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
+
+  validates :store, :first_name, :last_name, presence: true
+  validates :hourly_rate, presence: true, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
 
 end
